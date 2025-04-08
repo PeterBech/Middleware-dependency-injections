e2c4e602d9847204d6b4977cce4fb3f802aa94c2
PeterBech <pibm90@gmail.com>
2025-04-08 10:08:35 +0200

    opgave 1 færdig

---
+++ MinBlazorApp/MinBlazorApp/Middleware/LoggingMiddleware.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Threading.Tasks;
+
+namespace MinBlazorApp.Middleware
+{
+    public class LoggingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public LoggingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            // Log før request behandles
+            Console.WriteLine($"Request modtaget: {DateTime.Now} - {context.Request.Path}");
+
+            // Kald næste middleware i pipelinen
+            await _next(context);
+
+            // Log efter request er behandlet
+            Console.WriteLine($"Response sendt: {DateTime.Now} - Statuskode: {context.Response.StatusCode}");
+        }
+    }
+
+}
---
+++ MinBlazorApp/MinBlazorApp/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace MinBlazorApp.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseLogging(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<LoggingMiddleware>();
+        }
+    }
+}
